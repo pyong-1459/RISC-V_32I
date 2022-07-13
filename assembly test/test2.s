a4bb0b235182b8c37812ecf9870dfb0a20d35b89
pyong <58324470+pyong-1459@users.noreply.github.com>
2022-07-13 16:01:07 +0900

    Update test2.s

--- assembly test/test2.s
+++ assembly test/test2.s
@@ -6,9 +6,9 @@ addi a3, zero, 0x006
 addi a7, zero, 0x008
 
 shift:
-	sll a4, a0, a2
-	srl a5, a1, a2
-	sra a6, a1, a2
+    sll a4, a0, a2
+    srl a5, a1, a2
+    sra a6, a1, a2
     slt t1, a4, a7
     slti t2, a2, 0x003
     sltu t3, a6, zero
@@ -17,13 +17,13 @@ shift:
     sw a5, 0x004(t0)
     sw a6, 0x008(t0)
     addi t0, t0, 0x00C
-	addi a2, a2, 0x001
+    addi a2, a2, 0x001
     bge a3, a2, shift
     add t5, zero, t0
    	jal ra, logic
     
 logic:
-	addi a4, zero, -0x001
+    addi a4, zero, -0x001
     and a5, a4, a0
     xor a6, a4, a0
     or  a7, a4, a0
